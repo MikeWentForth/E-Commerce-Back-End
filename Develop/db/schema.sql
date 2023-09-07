c739888e1cdfff7b588c1a4dd602d1f8e2d6f589
MikeWentForth <themikewallace@gmail.com>
2023-09-07 11:52:53 -0700

    updated JS file with 1st table

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,46 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+USE ecommerce_db;
+
+-- CREATE CATEGORY TABLE
+CREATE TABLE Category
+(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    category_name VARCHAR(100) NOT NULL
+);
+
+-- CREATE PRODUCT TABLE
+
+CREATE TABLE Product
+(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(100) NOT NULL,
+    price DECIMAL(10,2) NOT NULL, -- DECIMAL(Max digits, digits after decimal point)
+    stock INT NOT NULL DEFAULT 10,
+    category_id INT,
+    -- VALIDATE price is decimal XXXXX
+    -- VALIDATE stock is numeric XXXXX
+    -- catergory_id reference category.id XXXXX
+);
+
+
+CREATE TABLE Tag
+(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tag_name VARCHAR(100) 
+);
+
+
+CREATE TABLE ProductTag
+(
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    product_id INT VARCHAR(100) NOT NULL,
+    tag_id INT
+    price DECIMAL(10,2) NOT NULL -- DECIMAL(Max digits, digits after decimal point)
+    stock INT NOT NULL AUTO_INCREMENT
+    category_id INT 
+);
+
+
+-- CREATE 
