b4184a8bc7bf8b8cea871a7fe74fb1f4bc500f92
MikeWentForth <themikewallace@gmail.com>
2023-09-10 20:53:36 -0700

    created mysql schema

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -24,7 +24,7 @@ CREATE TABLE Product
     category_id INT,
     -- VALIDATE price is decimal XXXXX
     -- VALIDATE stock is numeric XXXXX
-    -- catergory_id reference category.id XXXXX
+    FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
 );
 
 
@@ -38,12 +38,28 @@ CREATE TABLE Tag
 CREATE TABLE ProductTag
 (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    product_id INT VARCHAR(100) NOT NULL,
-    tag_id INT
-    price DECIMAL(10,2) NOT NULL -- DECIMAL(Max digits, digits after decimal point)
-    stock INT NOT NULL AUTO_INCREMENT
-    category_id INT 
+    product_id INT NOT NULL,
+    tag_id INT,
+    FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
+    FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
 );
 
 
 -- CREATE 
+
+-- GIVEN a functional Express.js API
+
+-- WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+-- THEN I am able to connect to a database using Sequelize
+
+-- WHEN I enter schema and seed commands
+-- THEN a development database is created and is seeded with test data
+
+-- WHEN I enter the command to invoke the application
+-- THEN my server is started and the Sequelize models are synced to the MySQL database
+
+-- WHEN I open API GET routes in Insomnia Core for categories, products, or tags
+-- THEN the data for each of these routes is displayed in a formatted JSON
+
+-- WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
+-- THEN I am able to successfully create, update, and delete data in my database
--- Develop/server.js
+++ Develop/server.js
@@ -19,10 +19,17 @@ app.listen(PORT, () => {
 // GIVEN a functional Express.js API
 
 // WHEN I add my database name, MySQL username, and MySQL password to an environment variable file
+// ... added to .env file DONE
 // THEN I am able to connect to a database using Sequelize
 
 // WHEN I enter schema and seed commands
 // THEN a development database is created and is seeded with test data
+// Instructions say to use MYSQL shell commands to run the schema.sql file.
+// To start MYSQL: XXXXXXXX
+// To login to MYSQL: mysql -u root -p .....
+// To run the schema file: source [path to file]
+// To "seed" use npm run seed (it is in the package.json file)
+
 
 // WHEN I enter the command to invoke the application
 // THEN my server is started and the Sequelize models are synced to the MySQL database
@@ -32,3 +39,18 @@ app.listen(PORT, () => {
 
 // WHEN I test API POST, PUT, and DELETE routes in Insomnia Core
 // THEN I am able to successfully create, update, and delete data in my database
+
+
+/*
+MySQL schema is created. schema.sql
+Problems when attempting npm run seed:
+  Sequelize is trying to drop tables, but can't due to foreign key constraints.
+  Attempted a workaround by ignoring foreign key constraints during the sync step.
+  Now proceeding, but sequelize isn't seeing all of the table fields
+  AND is not seeding the data from the seed files.
+
+Also, not sure how to incorporate the validation steps in the schema.sql.
+
+
+
+*/
